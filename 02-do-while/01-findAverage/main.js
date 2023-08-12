5f00462ac72fa418ea9e8406a208ec85d9020bb9
maiprapawin <maiprapawin@gmail.com>
2023-08-12 11:54:57 +0700

    finished: jslab04-loop (ver.2)

--- 02-do-while/01-findAverage/main.js
+++ 02-do-while/01-findAverage/main.js
@@ -1,3 +1,19 @@
+// - ให้เขียนโค้ดเพื่อรับ input มาเป็นตัวเลข
+// - โดยให้รับมาเรื่อยๆ จนกว่า ผู้ใช้จะพิมพ์ 0, เลขลบ, String ว่าง, กด cancel หรือ esc จึงหยุดรับตัวเลข
+// - ให้หาผลรวมและค่าเฉลี่ยของเลขที่ผู้ใช้งานกรอกเข้ามา
+
+// ```js
+// /*
+// GET : Input
+// CHECK : number
+// IF number
+//     ADD number to sum
+//     ADD 1 to count
+//     BACK TO LINE 7
+// ELSE
+//     PRINT sum
+//     PRINT average
+
 let num = "";
 let isEmpty;
 let isNan;
@@ -20,3 +36,6 @@ do {
   average = sum / count;
   count++;
 } while (num != isEmpty && num != isNan && num != zeroMinus);
+
+console.log(sum);
+console.log(average);
--- 03-forLoop/06-Interest/main.js
+++ 03-forLoop/06-Interest/main.js
@@ -1,3 +1,20 @@
-interestRate = 2.5;
-balance = 100_000;
-newBalance = balance + balance * (interestRate / 100);
+// - ให้คำนวณเงินฝากสะสมโดยปีที่ 1 มีเงินเริ่มต้น 100,000 ธนาคารให้ดอกเบี้ยร้อยละ 2.5 ต่อปี
+// - โดยจ่ายดอกเบี้ยปีละครั้ง ดอกเบี้ยที่ได้จะสะสมและทบยอดไปในเงินฝากปีถัดไป
+// - เมื่อครบ 10 ปี จะมียอดเงินฝากสะสมเท่าใด (ไม่มีการฝากเพิ่มและถอนออก)
+
+/////////////////////////////////////////////////////////////
+
+// Year 1
+let interestRate = 2.5;
+let balance = 100_000;
+let newBalance = balance + balance * (interestRate / 100);
+// console.log(`Earns ${newBalance} baht in year 1`);
+
+// Year 2 - 10
+for (let year = 2; year <= 10; year++) {
+  balance = newBalance;
+  newBalance = balance + balance * (interestRate / 100);
+  console.log(newBalance);
+}
+
+// Year 10 = เงินฝากสะสมรวม 128,008.45 baht
